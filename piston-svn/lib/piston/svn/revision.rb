9f5c649e754133ac3ce09bfae7b5c972568f7135
François Beausoleil <francois@teksol.info>
2008-03-13 15:03:35 -0400

    Implemented Piston::Svn::Revision#remember_values.

--- piston-svn/lib/piston/svn/revision.rb
+++ piston-svn/lib/piston/svn/revision.rb
@@ -8,6 +8,15 @@ module Piston
       def checkout_to(path)
         svn :checkout, "--quiet", "--revision", revision, path
       end
+
+      def remember_values
+        str = svn(:info, "--revision", revision, repository.url)
+        raise Piston::Svn::Client::Failed, "Could not get 'svn info' from #{repository.url} at revision #{revision}" if str.nil? || str.chomp.strip.empty?
+        info = YAML.load(str)
+        { Piston::Svn::UUID => info["Repository UUID"],
+          Piston::Svn::ROOT => info["URL"],
+          Piston::Svn::REMOTE_REV => info["Revision"]}
+      end
     end
   end
 end
---
+++ piston-svn/test/test_revision_rememberance.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + "/test_helper"
+require "pathname"
+
+class TestRevisionRememberance < Test::Unit::TestCase
+  def setup
+    @wcdir = Pathname.new("tmp/wc")
+    @wcdir.mkdir rescue nil
+    @repos = mock("repository")
+    @repos.stubs(:url).returns("http://a.repos.com/svn/trunk")
+
+    @info = {"Path" => @wcdir.realpath,
+        "URL" => "http://a.repos.com/svn/trunk",
+        "Repository Root" => "http://a.repos.com/svn",
+        "Repository UUID" => "some-long-uuid",
+        "Revision" => "9283",
+        "Node Kind" => "directory",
+        "Schedule" => "normal",
+        "Last Changed Author" => "me",
+        "Last Changed Rev" => "9283",
+        "Last Changed Date" => "2008-03-11 20:44:24 -0400 (Tue, 11 Mar 2008)"}
+  end
+
+  def teardown
+    @wcdir.rmtree rescue nil
+  end
+
+  def test_remembers_repos_uuid
+    rev = new_revision("HEAD")
+    rev.expects(:svn).with(:info, "--revision", "HEAD", @repos.url).returns(@info.to_yaml)
+    assert_equal "some-long-uuid", rev.remember_values[Piston::Svn::UUID]
+  end
+
+  def test_remembers_repos_url
+    rev = new_revision("HEAD")
+    rev.expects(:svn).with(:info, "--revision", "HEAD", @repos.url).returns(@info.to_yaml)
+    assert_equal @repos.url, rev.remember_values[Piston::Svn::ROOT]
+  end
+
+  def test_remembers_repos_revision
+    rev = new_revision("HEAD")
+    rev.expects(:svn).with(:info, "--revision", "HEAD", @repos.url).returns(@info.to_yaml)
+    assert_equal "9283", rev.remember_values[Piston::Svn::REMOTE_REV]
+  end
+
+  private
+  def new_revision(revision)
+    Piston::Svn::Revision.new(@repos, revision)
+  end
+end

dc4c269571ce27fe98da09e33946b7c00c806a35
François Beausoleil <francois@teksol.info>
2008-03-13 16:26:51 -0400

    New samples/import.rb to ensure an import will run fine.

---
+++ piston-svn/samples/import.rb
@@ -0,0 +1,37 @@
+require "pathname"
+require "logger"
+
+@root = Pathname.new(File.dirname(__FILE__)).parent
+
+$:.unshift @root + "lib"
+$:.unshift @root + "../piston-core/lib"
+
+require "piston"
+require "piston/repository"
+require "piston/working_copy"
+require "piston/svn"
+require "piston/svn/client"
+require "piston/svn/repository"
+require "piston/svn/working_copy"
+require "piston/svn/revision"
+
+PISTON_DEFAULT_LOGGER = Logger.new(STDOUT)
+Piston::Repository.logger = Piston::WorkingCopy.logger = PISTON_DEFAULT_LOGGER
+def logger; PISTON_DEFAULT_LOGGER; end
+include Piston::Svn::Client
+
+(@root + "tmp/repos").rmtree rescue nil
+(@root + "tmp/wc").rmtree rescue nil
+(@root + "tmp/.xlsuite.tmp").rmtree rescue nil
+
+svnadmin :create, @root + "tmp/repos"
+svn :checkout, "file://#{(@root + 'tmp/repos').realpath}", @root + "tmp/wc"
+
+repos = Piston::Svn::Repository.new("http://svn.xlsuite.org/trunk/lib")
+rev = repos.at(:head)
+rev.checkout_to(@root + "tmp/.xlsuite.tmp")
+wc = Piston::Svn::WorkingCopy.new(@root + "tmp/wc/vendor")
+wc.create
+wc.copy_from(rev)
+wc.remember(rev.remember_values)
+wc.finalize

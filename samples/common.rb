8324aa04d5d3c9b95c13fd7a2bf2f5d0567d04b7
François Beausoleil <francois@teksol.info>
2008-03-15 14:34:40 -0400

    Updated samples to test svn/svn and svn/git.

---
+++ samples/common.rb
@@ -0,0 +1,19 @@
+# Common code between all sample files.
+# Setup the environment.
+require "pathname"
+require "logger"
+
+@root = Pathname.new(File.dirname(__FILE__)).parent
+$:.unshift @root + "lib"
+
+require "piston"
+require "piston/svn"
+require "piston/git"
+
+PISTON_DEFAULT_LOGGER = Logger.new(STDOUT)
+Piston::Repository.logger = Piston::WorkingCopy.logger = PISTON_DEFAULT_LOGGER
+def logger; PISTON_DEFAULT_LOGGER; end
+
+include Piston::Svn::Client
+include Piston::Git::Client
+
--- samples/import_svn_git.rb
+++ samples/import_svn_git.rb
@@ -1,19 +1,24 @@
 #!/usr/bin/env ruby
 #
-# Import an SVN repository into an SVN working copy.
+# Import an SVN repository into a Git working copy.
 require File.dirname(__FILE__) + "/common"
 
 (@root + "tmp/repos").rmtree rescue nil
 (@root + "tmp/wc").rmtree rescue nil
 (@root + "tmp/.xlsuite.tmp").rmtree rescue nil
 
-svnadmin :create, @root + "tmp/repos"
-svn :checkout, "file://#{(@root + 'tmp/repos').realpath}", @root + "tmp/wc"
+(@root + "tmp/wc").mkpath
+File.open(@root + "tmp/wc/README", "wb") {|f| f.write "Hello World"}
+Dir.chdir(@root + "tmp/wc") do
+  git :init
+  git :add, "."
+  git :commit, "-m", "initial"
+end
 
 repos = Piston::Svn::Repository.new("http://svn.xlsuite.org/trunk/lib")
 rev = repos.at(:head)
 rev.checkout_to(@root + "tmp/.xlsuite.tmp")
-wc = Piston::Svn::WorkingCopy.new(@root + "tmp/wc/vendor")
+wc = Piston::Git::WorkingCopy.new(@root + "tmp/wc/vendor")
 wc.create
 wc.copy_from(rev)
 wc.remember(rev.remember_values)

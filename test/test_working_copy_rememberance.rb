c9907db998f58f0efcfc6e7df52c1e3eff9ebe41
François Beausoleil <francois@teksol.info>
2008-03-22 21:45:27 -0400

    Moved #test_writes_values_as_yaml_under_handler_key from the svn WC tests to the main Piston WC tests.

--- test/test_working_copy_rememberance.rb
+++ test/test_working_copy_rememberance.rb
@@ -2,23 +2,30 @@ require File.dirname(__FILE__) + "/test_helper"
 
 class TestWorkingCopyRememberance < Test::Unit::TestCase
   def setup
-    @wcpath = Pathname.new("tmp/wc")
-    @wcpath.rmtree rescue nil
-    @wcpath.mkpath
-    @wc = Piston::WorkingCopy.new(@wcpath)
+    @wcdir = Pathname.new("tmp/wc")
+    @wcdir.rmtree rescue nil
+    @wcdir.mkpath
+    @wc = Piston::WorkingCopy.new(@wcdir)
   end
 
   def teardown
-    @wcpath.rmtree rescue nil
+    @wcdir.rmtree rescue nil
   end
 
   def test_remember_generates_piston_yml_file_in_wc
     @wc.remember("a" => "b")
-    assert((@wcpath + ".piston.yml").exist?, "tmp/wc/.piston.yml file doesn't exist")
+    assert((@wcdir + ".piston.yml").exist?, "tmp/wc/.piston.yml file doesn't exist")
+  end
+
+  def test_writes_values_as_yaml_under_handler_key
+    expected = {"a" => "b"}
+    @wc.remember(expected)
+    actual = YAML.load((@wcdir + ".piston.yml").read)
+    assert_equal expected, actual["handler"]
   end
 
   def test_remember_calls_after_remember_with_path_to_piston_yml_file
-    @wc.expects(:after_remember).with(@wcpath + ".piston.yml")
+    @wc.expects(:after_remember).with(@wcdir + ".piston.yml")
     @wc.remember("a" => "b")
   end
 end

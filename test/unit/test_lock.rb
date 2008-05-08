2785518ba12c378a85abf6db73a384a3d9ff7b6b
Marcos Tapajos <tapajos@gmail.com>
2008-05-08 15:42:03 -0300

    Refactoring.

--- test/unit/test_lock.rb
+++ test/unit/test_lock.rb
@@ -2,38 +2,47 @@ require File.dirname(__FILE__) + "/../test_helper"
 
 class TestImport < Test::Unit::TestCase
   
+  def setup
+    @values_mock = {:lock => "false", "handler" => { :repository => "repository" }}
+    @wc_mock = mock("WorkingCopy")
+  end
+  
   def test_run
-    values_mock = {:lock => "false", "handler" => { :repository => "repository" }}
-    
-    wc_mock = mock("WorkingCopy")
-    wc_mock.expects(:exist?).returns(true)
-    wc_mock.expects(:pistonized?).returns(true)
-    wc_mock.expects(:recall).returns(values_mock)
-    wc_mock.expects(:finalize).returns(values_mock)
-    wc_mock.expects(:remember).with(values_mock, values_mock["handler"]).returns(values_mock)
-    Piston::WorkingCopy.expects(:guess).with("directory").returns(wc_mock)                                   
-    get_piston_lock.run(true)
+    run_and_verify do
+      @wc_mock.expects(:exist?).returns(true)
+      @wc_mock.expects(:pistonized?).returns(true)
+      @wc_mock.expects(:recall).returns(@values_mock)
+      @wc_mock.expects(:finalize).returns(@values_mock)
+      @wc_mock.expects(:remember).with(@values_mock, @values_mock["handler"]).returns(@values_mock)
+    end
   end
   
   def test_run_when_directory_not_exist
-    values_mock = {:lock => "false", "handler" => { :repository => "repository" }}
-    wc_mock = mock("WorkingCopy")
-    wc_mock.expects(:exist?).returns(false)
-    Piston::WorkingCopy.expects(:guess).with("directory").returns(wc_mock)                                   
-    assert_raise(Piston::WorkingCopy::NotWorkingCopy) { get_piston_lock.run(true) }
+    assert_raise(Piston::WorkingCopy::NotWorkingCopy) do
+      run_and_verify do
+        @wc_mock.expects(:exist?).returns(false)
+      end
+    end
   end
   
   def test_run_when_directory_exist_but_yml_not_exit
-    values_mock = {:lock => "false", "handler" => { :repository => "repository" }}
-    wc_mock = mock("WorkingCopy")
-    wc_mock.expects(:exist?).returns(true)
-    wc_mock.expects(:pistonized?).returns(false)
-    Piston::WorkingCopy.expects(:guess).with("directory").returns(wc_mock)                                   
-    assert_raise(Piston::WorkingCopy::NotWorkingCopy) { get_piston_lock.run(true) }
+    assert_raise(Piston::WorkingCopy::NotWorkingCopy) do
+      run_and_verify do
+        @wc_mock.expects(:exist?).returns(true)
+        @wc_mock.expects(:pistonized?).returns(false)
+      end
+    end
+  end
+  
+  private
+  
+  def run_and_verify
+    yield
+    get_piston_lock.run(true)
   end
   
   def get_piston_lock
-    
+    Piston::WorkingCopy.expects(:guess).with("directory").returns(@wc_mock)                                   
     Piston::Commands::Lock.new(  :wcdir => "directory",
                                        :verbose => "verbose",
                                        :quiet => "quiet",

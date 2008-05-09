3fa9e41332db8efbbe44942aec704f024ee13e7f
François Beausoleil <francois@teksol.info>
2008-05-09 11:18:48 -0400

    Piston::Commands::LockUnlock#run accepts both the wcdir and lock/unlock state as parameters.
    Wrote a new test to ensure unlock does the right thing.

--- lib/piston/commands/lock_unlock.rb
+++ lib/piston/commands/lock_unlock.rb
@@ -5,14 +5,16 @@ module Piston
     class LockUnlock < Piston::Commands::Base
       attr_reader :options
 
-      def run(lock)
-        working_copy = Piston::WorkingCopy.guess(options[:wcdir])
+      def run(wcdir, lock)
+        working_copy = Piston::WorkingCopy.guess(wcdir)
         raise Piston::WorkingCopy::NotWorkingCopy if !working_copy.exist? || !working_copy.pistonized?
 
         values = working_copy.recall
         values["lock"] = lock
         working_copy.remember(values, values["handler"])
         working_copy.finalize
+
+        logger.info "Locked #{working_copy} against automatic updates"
       end
     end
   end
--- test/unit/test_lock_unlock.rb
+++ test/unit/test_lock_unlock.rb
@@ -2,17 +2,28 @@ require File.dirname(__FILE__) + "/../test_helper"
 
 class TestLockUnlock < Test::Unit::TestCase
   def setup
-    @values = {:lock => "false", "handler" => { :repository => "repository" }}
+    @values = {"lock" => false}
+    @wcdir = "tmp/wcdir"
     @wc = mock("WorkingCopy")
   end
   
-  def test_run
-    run_and_verify do
+  def test_lock_working_copy
+    run_and_verify(true) do
       @wc.expects(:exist?).returns(true)
       @wc.expects(:pistonized?).returns(true)
       @wc.expects(:recall).returns(@values)
       @wc.expects(:finalize).returns(@values)
-      @wc.expects(:remember).with(@values, @values["handler"]).returns(@values)
+      @wc.expects(:remember).with(@values.merge("lock" => true), @values["handler"]).returns(@values)
+    end
+  end
+
+  def test_unlock_working_copy
+    run_and_verify(false) do
+      @wc.expects(:exist?).returns(true)
+      @wc.expects(:pistonized?).returns(true)
+      @wc.expects(:recall).returns(@values)
+      @wc.expects(:finalize).returns(@values)
+      @wc.expects(:remember).with(@values.merge("lock" => false), @values["handler"]).returns(@values)
     end
   end
   
@@ -34,14 +45,14 @@ class TestLockUnlock < Test::Unit::TestCase
   end
 
   private
-  def run_and_verify
+  def run_and_verify(lock=true)
     yield
-    piston_lock.run(true)
+    lock_unlock_command.run(@wcdir, lock)
   end
 
-  def piston_lock
-    Piston::WorkingCopy.expects(:guess).with("directory").returns(@wc)                                   
-    Piston::Commands::LockUnlock.new(:wcdir => "directory", :verbose => "verbose",
+  def lock_unlock_command
+    Piston::WorkingCopy.expects(:guess).with(@wcdir).returns(@wc)
+    Piston::Commands::LockUnlock.new(:verbose => "verbose",
                                      :quiet => "quiet", :force => "force")
   end
 end

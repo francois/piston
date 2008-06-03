26b5edfdcd1af29746302d71eb0bc73d941f58a4
François Beausoleil <francois@teksol.info>
2008-06-03 12:11:26 -0400

    Always get the sha1 of the commitable thing we checked out.

--- lib/piston/git/commit.rb
+++ lib/piston/git/commit.rb
@@ -41,10 +41,8 @@ module Piston
         Dir.chdir(@dir) do
           logger.debug {"in dir #{@dir}"}
           git(:checkout, "-b", "my-#{revision}", revision)
-          if revision == "HEAD" then
-            response = git(:log, "-n", "1")
-            @revision = $1 if response =~ /commit\s+([a-f\d]{40})/i
-          end
+          response = git(:log, "-n", "1")
+          @revision = $1 if response =~ /commit\s+([a-f\d]{40})/i
         end
 
         def remember_values
--- test/unit/git/commit/test_checkout.rb
+++ test/unit/git/commit/test_checkout.rb
@@ -12,6 +12,7 @@ class TestGitCommitCheckout < Test::Unit::TestCase
     @commit = Piston::Git::Commit.new(@repos, @sha1)
     @commit.expects(:git).with(:clone, @repos.url, @reposdir)
     @commit.expects(:git).with(:checkout, "-b", "my-#{@sha1}", @sha1)
+    @commit.expects(:git).with(:log, "-n", "1").returns("commit 922b12a6bcbb6f6a2cec60bcf5de17118086080a\nAuthor: Fran\303\247ois Beausoleil <francois@teksol.info>\nDate:   Fri Mar 14 13:28:41 2008 -0400\n\n    Changed how dependencies are found and managed, by using config/requirements.rb everywhere.\n    \n    Updated test/test_helper.rb where appropriate.\n")
     Dir.expects(:chdir).with(@reposdir).yields
     @commit.checkout_to(@reposdir)
   end

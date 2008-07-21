d76859a8697ed2e050857b3893db286b584c7ddc
François Beausoleil <francois@teksol.info>
2008-07-21 14:39:42 -0400

    Use ActiveSupport's #constantize call, instead of using the old removed version.

--- lib/piston/commands/import.rb
+++ lib/piston/commands/import.rb
@@ -17,7 +17,7 @@ module Piston
         if repository_type then
           logger.info {"Forced repository type to #{repository_type}"}
           repository_class_name = "Piston::#{repository_type.downcase.capitalize}::Repository"
-          repository_class = constantize(repository_class_name)
+          repository_class = repository_class_name.constantize
           repository_class.new(repository_url)
         else
           logger.info {"Guessing the repository type"}

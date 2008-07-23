d7a2820f48c1ee38a239f110fbbe91a9971f7462
François Beausoleil <francois@teksol.info>
2008-07-22 20:39:29 -0400

    Piston::Commands::Update should not rescue and throw away the exception.

--- lib/piston/commands/update.rb
+++ lib/piston/commands/update.rb
@@ -29,7 +29,7 @@ module Piston
           working_copy.update(from_revision, to_revision, tmpdir)
           working_copy.remember({:repository_url => repository.url, :lock => options[:lock],
               :repository_class => repository.class.name}, to_revision.remember_values)
-        rescue
+        ensure
           logger.debug {"Removing temporary directory: #{tmpdir}"}
           tmpdir.rmtree rescue nil
         end

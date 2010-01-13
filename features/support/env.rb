22e594e2e6f939eacf6cb2244d22f35cda48c0ef
François Beausoleil <francois@teksol.info>
2010-01-12 20:18:27 -0500

    Removing unicode support: apparently not in Cucumber 0.6.1

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,3 @@
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
 require "pathname"
 require "fileutils"
 require File.dirname(__FILE__) + "/svn"
